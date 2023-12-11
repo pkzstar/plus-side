6c74b64be8f3e27f78b04fc3f5b66bc0f9b845b8
pkzstar <iceclimberelite@hotmail.com>
2023-12-11 02:49:08 -0600

    dropdown year

---
+++ script.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    function goToNewPage()
+    {
+        let url = document.getElementById('Past-Events-Drop').value;
+        if(url != 'Past-Events.html') {
+            window.location = url;
+        }
+    }
+</script>
