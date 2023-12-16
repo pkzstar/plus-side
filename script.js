862da2b2afb7abe6868428c5ad5dcbd48c3c111f
pkzstar <iceclimberelite@hotmail.com>
2023-12-16 03:39:51 -0600

    imgs

--- script.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-    function goToNewPage()
-    {
-        let url = document.getElementById('Past-Events-Drop').value;
-        if(url != 'Past-Events.html') {
-            window.location = url;
-        }
-    }
-</script>
