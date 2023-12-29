ecd12eaa006d8a9a049611940826a67fa25a41cd
pkzstar <iceclimberelite@hotmail.com>
2023-12-29 14:17:21 -0600

    g5

---
+++ script.js
@@ -0,0 +1,17 @@
+function showArticleImage() {  
+   const reImg = document.getElementById('reImg');
+   const imgContainer = document.getElementById('imgContainer');
+   const imgCloseButton = document.getElementById('imgCloseButton');
+   imgCloseButton.style.display = "unset";
+   reImg.style.display = "none";
+}
+function closeImg () {
+    imgCloseButton.style.display = "none";
+    reImg.style.display = "unset";
+  }
+  if (reImg.style.display = "unset") {
+    imgCloseButton.style.display = "none";
+  }
+  if (reImg.style.display = "unset") {
+    imgCloseButton.style.display = "none";
+  }
