347607ce843a65fafe0f178e5ccd3dc06b88314b
pkzstar <iceclimberelite@hotmail.com>
2024-03-28 01:19:27 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: 'script.js',  // Path to your main JavaScript file
+  entry: './script.js',  // Path to your main JavaScript file
   output: {
     path: path.resolve(__dirname, 'dist'),  // Output directory for bundled files
     filename: 'bundle.js',  // Output filename
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',  // Path to your HTML template
+      template: './index.html',  // Path to your HTML template
       filename: 'index.html',  // Output HTML filename
     }),
   ],
