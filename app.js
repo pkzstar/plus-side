0de56ed10bbc8b1d2c591043cb6c18b2ccf2d837
pkzstar <iceclimberelite@hotmail.com>
2024-04-11 17:48:14 -0500

    test

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { client, connectToDatabase, closeDatabaseConnection } = require('./db'); // Import connectToDatabase and closeDatabaseConnection
+const { client, connectToDatabase, closeDatabaseConnection } = require('./server.js'); // Import connectToDatabase and closeDatabaseConnection
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,40 +12,22 @@ app.use(bodyParser.json());
 // Connect to the database and create events table
 connectToDatabase().catch(error => console.error('Error:', error));
 
-
-app.get('/', async (req, res) => {
-  console.log("hello");
-  res.status(200).send('Data saved successfully');
-  // const { upcomingTitle, upcomingDate, upcomingLocation, upcomingUrl, upcomingImg, upcomingDescription } = req.body;
-
-  // const query = 'INSERT INTO events (title, date, location, url, image, description) VALUES ($1, $2, $3, $4, $5, $6)';
-  // const values = [upcomingTitle, upcomingDate, upcomingLocation, upcomingUrl, upcomingImg, upcomingDescription];
-
-  // try {
-  //   const result = await client.query(query, values);
-  //   console.log('Data saved successfully:', result);
-  //   res.status(200).send('Data saved successfully');
-  // } catch (err) {
-  //   console.error('Error executing query:', err);
-  //   res.status(500).send('Error saving data');
-  // }
-});
 app.post('/submitEvent', async (req, res) => {
   console.log("hello");
   res.status(200).send('Data saved successfully');
-  // const { upcomingTitle, upcomingDate, upcomingLocation, upcomingUrl, upcomingImg, upcomingDescription } = req.body;
+  const { upcomingTitle, upcomingDate, upcomingLocation, upcomingUrl, upcomingImg, upcomingDescription } = req.body;
 
-  // const query = 'INSERT INTO events (title, date, location, url, image, description) VALUES ($1, $2, $3, $4, $5, $6)';
-  // const values = [upcomingTitle, upcomingDate, upcomingLocation, upcomingUrl, upcomingImg, upcomingDescription];
+  const query = 'INSERT INTO events (title, date, location, url, image, description) VALUES ($1, $2, $3, $4, $5, $6)';
+  const values = [upcomingTitle, upcomingDate, upcomingLocation, upcomingUrl, upcomingImg, upcomingDescription];
 
-  // try {
-  //   const result = await client.query(query, values);
-  //   console.log('Data saved successfully:', result);
-  //   res.status(200).send('Data saved successfully');
-  // } catch (err) {
-  //   console.error('Error executing query:', err);
-  //   res.status(500).send('Error saving data');
-  // }
+  try {
+    const result = await client.query(query, values);
+    console.log('Data saved successfully:', result);
+    res.status(200).send('Data saved successfully');
+  } catch (err) {
+    console.error('Error executing query:', err);
+    res.status(500).send('Error saving data');
+  }
 });
 
 app.listen(port, () => {
