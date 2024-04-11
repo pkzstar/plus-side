f15a3c288a8833d8d91a17a544e58181efac9322
pkzstar <iceclimberelite@hotmail.com>
2024-04-11 18:05:10 -0500

    .

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { client, connectToDatabase, closeDatabaseConnection } = require('./server.js'); // Import connectToDatabase and closeDatabaseConnection
+const { client, connectToDatabase, closeDatabaseConnection } = require('./server'); // Correct the import path
 require('dotenv').config();
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000; // Use environment port or default to 4000
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Connect to the database and create events table
-connectToDatabase().catch(error => console.error('Error:', error));
+connectToDatabase()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch(error => {
+    console.error('Error connecting to the database:', error);
+    process.exit(1); // Exit the process if unable to connect to the database
+  });
 
 app.post('/submitEvent', async (req, res) => {
-  console.log("hello");
-  res.status(200).send('Data saved successfully');
   const { upcomingTitle, upcomingDate, upcomingLocation, upcomingUrl, upcomingImg, upcomingDescription } = req.body;
 
   const query = 'INSERT INTO events (title, date, location, url, image, description) VALUES ($1, $2, $3, $4, $5, $6)';
@@ -22,7 +27,7 @@ app.post('/submitEvent', async (req, res) => {
 
   try {
     const result = await client.query(query, values);
-    console.log('Data saved successfully:', result);
+    console.log('Data saved successfully:', result.rows);
     res.status(200).send('Data saved successfully');
   } catch (err) {
     console.error('Error executing query:', err);
@@ -34,7 +39,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// Close the database connection when the server is shutting down
+// Gracefully close the database connection when the server is shutting down
 process.on('SIGINT', async () => {
   console.log('Shutting down server...');
   await closeDatabaseConnection();
