e162c58fa78d57d199e09d913c343bf1634ad92f
pkzstar <iceclimberelite@hotmail.com>
2024-05-28 17:13:23 -0500

    massive directory changes, headernav stylings, style sheet conversions etc

--- past-events/past js/pastButtons2023.js
+++ past-events/past js/pastButtons2023.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
       <div class="upcoming-image-parent">
         <h1 id="pageHeader" class="textGradient">2023 PAST EVENTS</h1>
         <div>
-          <button class="calView"><a href="/Calendar.html">Calendar</a></button>
+        <button class="calView"><a href="../Calendar/CalView.html">Calendar</a></button>
           <div onclick="toggleDropdown(event)" style="display: inline-flex;" class="past-events-drop calView">
             <!-- Dropdown Button -->
             <button id="past-events-drop-btn" class="past-events-drop-btn">
